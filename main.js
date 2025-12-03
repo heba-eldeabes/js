638f416642d731c1c2a8296a778ace0181951d6f
heba-eldeabes <aldbashbh@gmail.com>
2025-12-03 20:46:46 +0200

    first commit

--- main.js
+++ main.js
@@ -258,35 +258,322 @@
 //  if ( a.slice(0,6).repeat(2)=== "ElzeroElzero"){
 //    console.log("Good");
 
+//  }  var user = "heba" ,age =23;
+//  console.log(user); 
+//  console.log(age);
+//  console.log(typeof('heba eldeabas')) ;
+//   console.log(typeof 3000) ;
+//   console.log(typeof 3000.78) ;
+//   console.log(typeof [10,13,14]);
+//   console.log(typeof ["ah","ad","fa"]); 
+//    console.log(typeof {name:"osama",age,country:"EG"});
+//     console.log(typeof true); 
+//     console.log(typeof false); 
+//     console.log(typeof undefined); 
+//     console.log(typeof null); 
+
+//  console.log(hallo); 
+//  hallo.innerhtml ="heba";
+
+
+//  console.log("zero Web school"); 
+//  console.log("zero Web 'school'"); 
+//  console.log('zero Web "school"');  
+//  console.log("zero Web \\school\\");  
+//  console.log("zero Web \"school\"");  
+//  console.log("zero Web \'school\'");  
+//  console.log("zero \nWeb \nschool");   
+
+
+//  let a ="we love";
+//  let b = "javaScript" ;
+
+//   console.log (a, b) ; 
+//   document.write  (a+" "+b) ; 
+ 
+//   let E ="we love";
+//  let R= "javaScript" ;
+//  let C ="And" ;
+//  let d ="programming"; 
+//  console.log (E +"  \"\" " +R +
+//     "\n" +C +" "+ d) ; 
+//     console.log(`${E} "" ${R} 
+// ${C} ${d} `) ; 
+
+
+
+
+// let thetitle="Elzero", theDescription ="Elzero web school" ,theDate ="25/10";
+
+//  let hi = 
+// `<div >
+
+//     <h3>hallo${thetitle}</h3>
+// <p>${theDescription}</p> 
+// <span>${theDate}</span>
+
+//   </div> `
+// ; 
+// document.write(hi.repeat(4)) ;
+ 
+//  console.log(10+3);
+// console.log(10+"osama"); 
+// console.log(10-3);
+// console.log(10-"osama"); 
+// console.log(typeof(NaN));
+// console.log(10*3);
+// console.log(10*-3); 
+// console.log(10/3); 
+// console.log(20/5);
+// console.log(10**3); 
+// console.log(20%5); 
+// console.log(21%5);
+// console.log(35%2); //remove 1 عشان ارجع عدد صحيح
+// console.log(+100);
+// console.log(+"100"); 
+// console.log(+"-100");
+//  console.log(+"-osama"); 
+//  console.log(+"100.89"); 
+//  console.log(+0xff); 
+//  console.log(+null); 
+//  console.log(+true);
+//  console.log(+false);
+
+
+//  console.log(-100);
+// console.log(-"100"); 
+// console.log(-"-100");
+//  console.log(-"-osama"); 
+//  console.log(-"100.89"); 
+//  console.log(-0xff); 
+//  console.log(-null); 
+//  console.log(-true);
+//  console.log(-false); 
+
+
+//  console.log(Number("100")); 
+
+
+
+
+//  let u ="10";
+//  let p=20;
+//  let s =true; 
+ 
+//  console.log (u+p); 
+//  console.log (+u+p);  
+//  console.log (Number(u)+p);  
+//  console.log (u-p);  
+//  console.log (s+u+p); 
+//  console.log (s+ +u+p);   
+
+//  console.log (+"");  
+//  console.log (""-2);   
+//  console.log (+false- +true);  
+
+
+
+
+
+
+//  let k =20; 
+//  k=k+30; 
+//  k=k+80; 
+//  k +=100;
+//  k-=30;
+//  k/=50; 
+ 
+
+// console.log(k);
+
+
+// let z="-100";
+// let h="20";
+// let f=30;
+// let g=true;
+
+
+
+// console.log(-z*+h); 
+// console.log(-z+ ++g * ++h + ++f); 
+
+
+
+// console.log (1000000);
+// console.log (1_000_000); 
+// console.log (1e6); 
+// console.log (10**6);  
+// console.log (1000000);
+// console.log (10 * 10 * 10* 10  *10 * 10 );
+// console.log (1000000.00); 
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE) 
+// console.log(100..toString()) 
+// console.log((100).toString()) 
+// console.log(100.10.toString())  
+// console.log(100.5555 .toFixed(2))  
+// console.log(100.5555 .toFixed())  
+// console.log(100.5545 .toFixed(2))  
+// console.log(parseInt("100 osama"))   
+// console.log(+"100 osama") 
+// console.log(Number("100 osama"))  
+// console.log(parseInt(" osama 100 "))  
+// console.log(parseFloat("100.500"))  
+// console.log(parseInt("100.500"))   
+// console.log(Number.isInteger("100" ))
+// console.log(Number.isInteger(100 ))
+// console.log(Number.isInteger(100.500 )) 
+// console.log(Number.isNaN(100 ))
+// console.log(Number.isNaN("100" ))
+// console.log(Number.isNaN( "osama" /100 ))
+//  console.log(Math.round(99.5)) ;
+//  console.log(Math.round(99.2)); 
+//   console.log(Math.ceil(99.2)); 
+//    console.log(Math.floor(99.9)); 
+//     console.log(Math.min(10 ,20,-100,90));
+//      console.log(Math.max(10 ,20,-100,90));
+//       console.log(Math.pow(2,4));
+//      console.log(Math.random()); 
+//          console.log(Math.trunc(99.5));
+
+
+ 
+// let a=1_00 ;
+// let b=2_00.5 ;
+// let c=1e2 ;
+// let d=2.4 ;
+ 
+// console.log(Math.min(a,b,c,d)); 
+ 
+//   console.log ( a**Math.floor(d)) 
+// console.log(Math.floor(d) ) 
+// console.log(Math.trunc(d)) 
+// console.log(Math.round(d))
+// console.log(parseInt(d)) 
+
+
+// let thename ="  Ahmed  ";
+// let thelist=[1,2,3,4,5]; 
+
+// console.log(thename);
+//   console.log(thename[1]) 
+
+//     console.log(thename.charAt(1)) 
+//       console.log(thename.length)
+//   console.log(thename.trim()) 
+//     console.log(thename.toUpperCase()) 
+//       console.log(thename.toLowerCase()) 
+
+//         console.log(thename.trim().charAt(2).toUpperCase()); 
+
+
+
+
+  
+   
+
+// let a= "Elzero web school" ; 
+
+//  console.log (a.slice(2,6).charAt(0).toUpperCase() );
+
+//  console.log (a.slice(-4,-3).toUpperCase().repeat(8))  
+ 
+
+//  console.log (a.substring(0,6).split());
+
+// console.log (a.substr(0,6)+" "+a.substr(11,6)) 
+
+
+// console.log(a.length); 
+
+
+//    console.log(a.charAt(0).toLowerCase()+ a.slice(1 ,a.length -1 ) .toUpperCase()+ a.substring(a.length -1).toLowerCase());
+
+
+
+ 
+//  if ( typeof"Elzero web school"=== typeof "34"){
+//    console.log("Good");
+
+//  } 
+
+//  if (a.charAt((a.indexOf("w"))).toUpperCase()==="W"){
+
+//    console.log("Good");
+//  }
+
+// if(`${a}`!=="string"){
+//  console.log("Good"); 
+
+// } 
+
+
+// if ( typeof"Elzero web school"=== typeof"number"){
+//    console.log("Good");
+
+//  } 
+
+ 
+//  if ( a.slice(0,6).repeat(2)=== "ElzeroElzero"){
+//    console.log("Good");
+
 //  } 
 
 
 
 
-let myfriends=["ahmed","nor", "eslam","fathy","mohamed"];
+// let myfriends=["ahmed","nor", "eslam","fathy","mohamed"];
  
-console.log(myfriends[0]);
-console.log(myfriends[1]);
-console.log(myfriends[2]);
-console.log(myfriends[3]);
-console.log(myfriends[4]);
+// console.log(myfriends[0]);
+// console.log(myfriends[1]);
+// console.log(myfriends[2]);
+// console.log(myfriends[3]);
+// console.log(myfriends[4]);
 
-for(let i = 0; i< myfriends.length; i++ ){
-console.log(myfriends[i]);
+// for(let i = 0; i< myfriends.length; i++ ){
+// console.log(myfriends[i]);
 
-}  
+// }  
 
 
-let myfriends1=[1,2,3,4,"ahmed","nor", 6,"eslam",9,"fathy","mohamed"];
+// let myfriends1=[1,2,3,4,"ahmed","nor", 6,"eslam",9,"fathy","mohamed"];
 
-let myname=[];
-for(let i = 0; i< myfriends1.length; i++ ){
+// let myname=[];
+// for(let i = 0; i< myfriends1.length; i++ ){
 
-if(typeof myfriends1[i]==="string"){ 
-  myname.push(myfriends1[i]);
+// if(typeof myfriends1[i]==="string"){ 
+//   myname.push(myfriends1[i]);
 
-} 
+// } 
 
-}  
- console.log(myname);
+// }  
+//  console.log(myname);
 
+
+ let products=["keyboard","mouse","pen","pad","monitor"];
+
+ let colors=["red","grean","black"];
+
+ let models=[2025,2026];
+
+
+ for (let i=0 ; i < products.length;i++){
+console.log("#".repeat(15));
+console.log(`#${products[i]}`);
+console.log("#".repeat(15));
+console.log("colors:"); 
+
+ for (let j=0 ; j < colors.length;j++){
+
+console.log(`-${colors[j]}`); 
+
+ }
+ for (let k=0 ; k < models.length;k++){
+
+console.log(models[k]); 
+
+ }
+
+
+ };
